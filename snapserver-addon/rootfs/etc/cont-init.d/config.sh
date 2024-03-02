8a4aa58c6c6bdea96abf49261bd1a883ffc681b8
alexyao2015 <33379584+alexyao2015@users.noreply.github.com>
2024-03-02 15:44:21 -0600

    fix config

--- snapserver-addon/rootfs/etc/cont-init.d/config.sh
+++ snapserver-addon/rootfs/etc/cont-init.d/config.sh
@@ -2,10 +2,11 @@
 # ==============================================================================
 # Ensure the config file exists
 # ==============================================================================
-readonly SNAPSERVER_CONFIG="/config"
+readonly SNAPSERVER_CONFIG_DIR="/config"
+readonly SNAPSERVER_CONFIG="${SNAPSERVER_CONFIG_DIR}/snapserver.conf"
 
-if ! bashio::fs.directory_exists "${SNAPSERVER_CONFIG}"; then
+if ! bashio::fs.file_exists "${SNAPSERVER_CONFIG}"; then
   bashio::log.info "Creating default configuration..."
-  mkdir -p "${SNAPSERVER_CONFIG}" || bashio::exit.nok "Could not create media folder: ${SNAPSERVER_CONFIG}"
-  cp /etc/snapserver.conf "${SNAPSERVER_CONFIG}/snapserver.conf" || bashio::exit.nok "Could not copy default configuration to: ${SNAPSERVER_CONFIG}"
+  mkdir -p "${SNAPSERVER_CONFIG_DIR}" || bashio::exit.nok "Could not create config folder: ${SNAPSERVER_CONFIG}"
+  cp /etc/snapserver.conf "${SNAPSERVER_CONFIG}" || bashio::exit.nok "Could not copy default configuration to: ${SNAPSERVER_CONFIG}"
 fi
