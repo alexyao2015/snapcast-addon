b2f5b289b8e1003c775b3e0108538a0159bdf182
alexyao2015 <33379584+alexyao2015@users.noreply.github.com>
2024-03-02 15:51:49 -0600

    fix ro config

--- snapserver-addon/rootfs/etc/cont-init.d/config.sh
+++ snapserver-addon/rootfs/etc/cont-init.d/config.sh
@@ -5,8 +5,8 @@
 readonly SNAPSERVER_CONFIG_DIR="/config"
 readonly SNAPSERVER_CONFIG="${SNAPSERVER_CONFIG_DIR}/snapserver.conf"
 
-# if ! bashio::fs.file_exists "${SNAPSERVER_CONFIG}"; then
-#   bashio::log.info "Creating default configuration..."
-#   mkdir -p "${SNAPSERVER_CONFIG_DIR}" || bashio::exit.nok "Could not create config folder: ${SNAPSERVER_CONFIG}"
-#   cp /etc/snapserver.conf "${SNAPSERVER_CONFIG}" || bashio::exit.nok "Could not copy default configuration to: ${SNAPSERVER_CONFIG}"
-# fi
+if ! bashio::fs.file_exists "${SNAPSERVER_CONFIG}"; then
+  bashio::log.info "Creating default configuration..."
+  mkdir -p "${SNAPSERVER_CONFIG_DIR}" || bashio::exit.nok "Could not create config folder: ${SNAPSERVER_CONFIG}"
+  cp /etc/snapserver.conf "${SNAPSERVER_CONFIG}" || bashio::exit.nok "Could not copy default configuration to: ${SNAPSERVER_CONFIG}"
+fi
